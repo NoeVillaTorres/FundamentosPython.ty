6114a75ffc0452fa856665f71239113839c96f6c
noe <noevdm2@hotmail.com>
2023-02-15 16:52:34 -0600

    avance de fundamentos

--- fundamentospython.py
+++ fundamentospython.py
@@ -1,5 +1,8 @@
 #esto es un comentario
 
+def nuevoTema(tema):
+    print("\n===",tema,"===\n")
+
 
 if __name__=="__main__":
     print("=== Operadores aritmeticos====")
@@ -26,15 +29,56 @@ print("(false or true):)",True or False)
 
 print("(true not true):)", not False)
 print("(false not false):)",not True)
+nuevoTema("Variables")
+variable1=10
+variable2=34.2
+variable3="pepe"
+variable4="hola"
 
-def nuevoTema(tema):
-    print("\===Nuevo tema===/")
+print(nuevoTema,variable1,variable2,variable3,variable4)
+nuevoTema("Enteros")
+w=125
+x=549848696
+y=-345
+z=0b0110011
+h=0xffa #entero en hexadecimal
 
-    nuevoTema("Variables")
-    variable1=10
-    variable2=34.2
-    variable3="pepe"
-    variable4="hola"
+print(w,type(w))
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print(h,type(h))
+nuevoTema("flotantes")
+x=1652.5
+y=0.0656652
+print(x,type(x))
+print(y,type(y))
+nuevoTema("Numeros imaginarios")
+x=-46j
+y=12+45j
+z=2j
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
 
-    print(variable1,variable2,variable3,variable4)
-    
+nuevoTema("Listas")
+a=[10,20.5,"python list"]
+print(a)
+a=["lista2",46,16.3j]
+print(a)
+
+nuevoTema("Tuplas")
+t=(25,"tupla",5.6)
+print(t)
+print(t[1])
+t[0]="Modificado"
+print(t)
+nuevoTema("Conjuntos")
+c={50,20,18,4,8,50}
+print(c)
+nuevoTema("Conjuntos")
+d={1:"valor1","2":45}
+print(d, type(d))
+
+
+
